a652fad05b7749d3601ca7ff813acf93d39c1dc2
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-01-07 06:50:26 -0500

    Fixed export links

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Schema = require('./src/data/Schema');
-const DataLoader = require('./src/data/DataLoader');
+const Schema = require('./src/core/Schema');
+const DataLoader = require('./src/core/DataLoader');
 const { eventEmitter: Emitter } = require('./src/service/event.service');
 
 module.exports = {
