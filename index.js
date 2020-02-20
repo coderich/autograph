1dc6be911a816ce225fc6351a7366df247763330
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-02-19 21:57:55 -0500

    Quin semi-permanent now

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Schema = require('./src/core/Schema');
 const Resolver = require('./src/core/Resolver');
+const Rule = require('./src/graphql/Rule');
+const Transformer = require('./src/graphql/Transformer');
 const { eventEmitter: Emitter } = require('./src/service/event.service');
 
 module.exports = {
   Schema,
   Resolver,
+  Rule,
+  Transformer,
   Emitter,
 };
