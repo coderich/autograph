2795a857448c8b245539583b2d7da418ec9350a4
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-02-23 14:51:51 -0500

    Fixed bug with pagination

--- src/service/data.service.js
+++ src/service/data.service.js
@@ -192,26 +192,99 @@ exports.filterDataByCounts = (resolver, model, data, countPaths) => {
 };
 
 exports.paginateResults = (results = [], pagination = {}) => {
-  const { before, after, first = Infinity, last = 0 } = pagination;
-  if (first < 0 || last < 0) throw new Error('Invalid first|last pagination');
+  const applyCursorsToEdges = (allEdges, before, after) => {
+    const edges = [...allEdges];
 
-  const totalCount = results.length;
-  const cursors = results.map(result => result.$$cursor);
-  const afterIndex = cursors.findIndex(cursor => Boolean(cursor >= after)); // Want edges after this index
-  let beforeIndex = cursors.findIndex(cursor => Boolean(cursor >= before)); // Want edges before this index
-  if (beforeIndex === -1) beforeIndex = Infinity;
-  const edges = results.slice(afterIndex + 1, beforeIndex);
-  const hasPreviousPage = Boolean(last ? (edges.length > last) : (after && afterIndex));
-  const hasNextPage = Boolean(first !== Infinity ? (edges.length > first) : (before && beforeIndex < results.length));
-  const slice = edges.slice(0, first).slice(-last);
+    if (after) {
+      const afterEdge = edges.findIndex(edge => edge.$$cursor === after);
+      if (afterEdge > -1) edges.splice(0, afterEdge + 1);
+    }
 
-  return Object.defineProperty(slice, '$$pageInfo', {
+    if (before) {
+      const beforeEdge = edges.findIndex(edge => edge.$$cursor === before);
+      if (beforeEdge > -1) edges.splice(beforeEdge);
+    }
+
+    return edges;
+  };
+
+  const edgesToReturn = (allEdges, before, after, first, last) => {
+    const edges = applyCursorsToEdges(allEdges, before, after);
+
+    if (first) {
+      if (first < 0) throw new Error();
+      if (edges.length > first) edges.splice(first);
+    }
+
+    if (last) {
+      if (last < 0) throw new Error();
+      if (edges.length > last) edges.splice(0, edges.length - last);
+    }
+
+    return edges;
+  };
+
+  const hasPreviousPage = (allEdges, before, after, first, last) => {
+    if (last) {
+      const edges = applyCursorsToEdges(allEdges, before, after);
+      return Boolean(edges.length > last);
+    }
+
+    if (after) {
+      const index = allEdges.findIndex(edge => edge.$$cursor <= after);
+      return Boolean(index > -1);
+    }
+
+    return false;
+  };
+
+  const hasNextPage = (allEdges, before, after, first, last) => {
+    if (first) {
+      const edges = applyCursorsToEdges(allEdges, before, after);
+      return Boolean(edges.length > first);
+    }
+
+    if (before) {
+      const index = allEdges.findIndex(edge => edge.$$cursor >= before);
+      return Boolean(index > -1);
+    }
+
+    return false;
+  };
+
+  const { before, after, first, last } = pagination;
+  const edges = edgesToReturn(results, before, after, first, last);
+
+  return Object.defineProperty(edges, '$$pageInfo', {
     value: {
-      startCursor: _.get(slice, '0.$$cursor', ''),
-      endCursor: _.get(slice, `${slice.length - 1}.$$cursor`, ''),
-      hasPreviousPage,
-      hasNextPage,
-      totalCount,
+      startCursor: _.get(edges, '0.$$cursor', ''),
+      endCursor: _.get(edges, `${edges.length - 1}.$$cursor`, ''),
+      hasPreviousPage: hasPreviousPage(results, before, after, first, last),
+      hasNextPage: hasNextPage(results, before, after, first, last),
+      totalCount: results.length,
     },
   });
+
+  // const { before, after, first = Infinity, last = 0 } = pagination;
+  // if (first < 0 || last < 0) throw new Error('Invalid first|last pagination');
+
+  // const totalCount = results.length;
+  // const cursors = results.map(result => result.$$cursor);
+  // const afterIndex = cursors.findIndex(cursor => Boolean(cursor >= after)); // Want edges after this index
+  // let beforeIndex = cursors.findIndex(cursor => Boolean(cursor >= before)); // Want edges before this index
+  // if (beforeIndex === -1) beforeIndex = Infinity;
+  // const edges = results.slice(afterIndex + 1, beforeIndex);
+  // const hasPreviousPage = Boolean(last ? (edges.length > last) : (after && afterIndex > 0));
+  // const hasNextPage = Boolean(first !== Infinity ? (edges.length > first) : (before && beforeIndex < results.length));
+  // const slice = edges.slice(0, first).slice(-last);
+
+  // return Object.defineProperty(slice, '$$pageInfo', {
+  //   value: {
+  //     startCursor: _.get(slice, '0.$$cursor', ''),
+  //     endCursor: _.get(slice, `${slice.length - 1}.$$cursor`, ''),
+  //     hasPreviousPage,
+  //     hasNextPage,
+  //     totalCount,
+  //   },
+  // });
 };
