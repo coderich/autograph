c0563719126fef460e74f8decc0eb5ebadd0a64f
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-05-18 00:00:50 -0400

    Reverted Type to simpler class

--- src/graphql/ast/Type.js
+++ src/graphql/ast/Type.js
@@ -3,29 +3,20 @@ const { getTypeInfo } = require('../../service/graphql.service');
 
 const scalars = ['ID', 'String', 'Float', 'Int', 'Boolean'];
 
-module.exports = class Type {
-  constructor(ast) {
-    this.node = new Node(ast.type);
-    this.node.getName = () => getTypeInfo(ast).name;
-    this.node.isArray = () => Boolean(getTypeInfo(ast).isArray);
-    this.node.isRequired = () => Boolean(getTypeInfo(ast).isRequired);
-    this.node.isScalar = () => scalars.indexOf(this.node.getName()) > -1;
-    return this.node;
+module.exports = class Type extends Node {
+  getName() {
+    return getTypeInfo(this.ast).name;
   }
 
-  // getName() {
-  //   return getTypeInfo(this.ast).name;
-  // }
+  isArray() {
+    return Boolean(getTypeInfo(this.ast).isArray);
+  }
 
-  // isArray() {
-  //   return Boolean(getTypeInfo(this.ast).isArray);
-  // }
+  isRequired() {
+    return Boolean(getTypeInfo(this.ast).isRequired);
+  }
 
-  // isRequired() {
-  //   return Boolean(getTypeInfo(this.ast).isRequired);
-  // }
-
-  // isScalar() {
-  //   return scalars.indexOf(this.getName()) > -1;
-  // }
+  isScalar() {
+    return scalars.indexOf(this.getName()) > -1;
+  }
 };
