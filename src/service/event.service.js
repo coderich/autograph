7c1f7ccdb9afd4d90f73b68ea172821e5afabe81
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-05-18 21:47:54 -0400

    reverted event.service

--- src/service/event.service.js
+++ src/service/event.service.js
@@ -3,16 +3,23 @@ const { ucFirst } = require('./app.service');
 
 // Event emitters
 const eventEmitter = new EventEmitter();
+const internalEmitter = new EventEmitter();
+const systemEvent = new EventEmitter().on('system', async (event, next) => {
+  const { type, data } = event;
+  await internalEmitter.emit(type, data);
+  await eventEmitter.emit(type, data);
+  next();
+});
 
 //
 exports.createSystemEvent = (name, event = {}, thunk = () => {}) => {
   const type = ucFirst(name);
 
-  return eventEmitter.emit(`pre${type}`, event).then(() => thunk()).then((result) => {
+  return systemEvent.emit('system', { type: `pre${type}`, data: event }).then(() => thunk()).then((result) => {
     event.result = result;
-    eventEmitter.emit(`post${type}`, event);
+    systemEvent.emit('system', { type: `post${type}`, data: event });
     return result;
   });
 };
-
 exports.eventEmitter = eventEmitter;
+exports.internalEmitter = internalEmitter;
