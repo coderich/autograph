d7098f9c87e1e1374342ea4648e91972a4c79f3e
Richard Livolsi <richard.livolsi@goziohealth.com>
2021-02-24 22:35:30 -0500

    handling embedded where clause

--- src/service/data.service.js
+++ src/service/data.service.js
@@ -93,7 +93,23 @@ exports.spliceEmbeddedArray = async (query, doc, key, from, to) => {
 };
 
 exports.resolveModelWhereClause = (resolver, model, where = {}, options) => {
+  // This is needed for where clause (but why!?!)
   if (where.id) where.id = map(where.id, v => model.idValue(v));
+
+  const resolveEmbeddedWhere = (ref, key, value) => {
+    const resolved = ensureArray(map(value, (obj) => {
+      return Object.entries(obj).reduce((p, [k, v]) => {
+        const f = ref.getFieldByName(k);
+
+        if (k === 'id') return Object.assign(p, { [k]: ref.idValue(v) });
+        if (f.isScalar()) return Object.assign(p, { [k]: v });
+        if (f.isEmbedded()) return Object.assign(p, { [k]: resolveEmbeddedWhere(f.getModelRef(), k, v) });
+        return Object.assign(p, { [k]: v });
+      }, {});
+    }));
+
+    return resolved.length > 1 ? resolved : resolved[0];
+  };
 
   // Construct
   const $where = Object.entries(where).reduce((prev, [key, value]) => {
@@ -109,6 +125,11 @@ exports.resolveModelWhereClause = (resolver, model, where = {}, options) => {
     if (modelRef && !field.isEmbedded()) {
       const ids = Promise.all(ensureArray(value).map(v => (isPlainObject(v) ? resolver.match(modelRef).where(v).options(options).many().then(docs => docs.map(doc => doc.id)) : Promise.resolve(v)))).then(results => _.uniq(_.flattenDeep(results)));
       return Object.assign(prev, { [key]: ids });
+    }
+
+    // You do not have a unit-test that tests this (BUT ITS NEEDED)
+    if (field.isEmbedded()) {
+      return Object.assign(prev, { [key]: resolveEmbeddedWhere(modelRef, key, value) });
     }
 
     return Object.assign(prev, { [key]: value });
