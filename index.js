cc79bab2f7c80900505e0b293a456469af7076ea
Richard Livolsi <richard.livolsi@goziohealth.com>
2022-06-21 14:47:55 -0400

    Adding core/Schema2 and exporting it

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Schema = require('./src/core/Schema');
+const Schema2 = require('./src/core/Schema2');
 const GraphQL = require('./src/core/GraphQL');
 const Resolver = require('./src/core/Resolver');
 const Rule = require('./src/core/Rule');
@@ -8,6 +9,7 @@ const { eventEmitter: Emitter } = require('./src/service/event.service');
 
 module.exports = {
   Schema,
+  Schema2,
   GraphQL,
   Resolver,
   Rule,
---
+++ src/core/Schema2.js
@@ -0,0 +1,46 @@
+const Model = require('../data/Model');
+const Schema = require('../graphql/ast/Schema2');
+const { identifyOnDeletes } = require('../service/schema.service');
+const { createSystemEvent } = require('../service/event.service');
+
+// Export class
+module.exports = class extends Schema {
+  constructor(schema, stores) {
+    super(schema);
+
+    // Create drivers
+    this.drivers = Object.entries(stores).reduce((prev, [key, value]) => {
+      const { Driver } = value;
+
+      return Object.assign(prev, {
+        [key]: {
+          dao: new Driver(value, this),
+          idKey: Driver.idKey,
+          idValue: Driver.idValue,
+        },
+      });
+    }, {});
+  }
+
+  setup() {
+    return createSystemEvent('Setup', this, () => {
+      const entities = this.models.filter(m => m.isEntity());
+
+      // Create model indexes
+      return Promise.all(entities.map(async (model) => {
+        const key = model.getKey();
+        const indexes = model.getIndexes();
+        const driver = model.getDriver();
+        if (driver.createCollection) await driver.createCollection(key);
+        return driver.createIndexes(key, indexes);
+      }));
+    });
+  }
+
+  initialize() {
+    super.initialize();
+    this.models = super.getModels().map(model => new Model(this, model, this.drivers[model.getDriverName()]));
+    this.models.forEach(model => model.referentialIntegrity(identifyOnDeletes(this.models, model)));
+    return this;
+  }
+};
