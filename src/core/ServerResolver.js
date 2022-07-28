e24a6d56b358eb38bb6ff239d33ae3c68d4d9423
Richard Livolsi <richard.livolsi@goziohealth.com>
2022-07-28 09:56:26 -0400

    Hydrating select fields apriori

--- src/core/ServerResolver.js
+++ src/core/ServerResolver.js
@@ -8,7 +8,7 @@ module.exports = class ServerResolver {
     this.count = ({ autograph }, model, args, query) => autograph.resolver.match(model).merge(args).count();
 
     // Mutations
-    this.create = ({ autograph }, model, { input, meta }, query) => autograph.resolver.match(model).meta(meta).select(query.fields).save(unrollGuid(autograph, model, input));
+    this.create = ({ autograph }, model, { input, meta }, query) => autograph.resolver.match(model).select(query.fields).meta(meta).save(unrollGuid(autograph, model, input));
     this.delete = ({ autograph }, model, { id: guid, meta }, query) => autograph.resolver.match(model).id(guidToId(autograph, guid)).select(query.fields).meta(meta).remove();
     this.update = ({ autograph }, model, { id: guid, meta, input }, query) => autograph.resolver.match(model).id(guidToId(autograph, guid)).select(query.fields).meta(meta).save(unrollGuid(autograph, model, input));
   }
--- src/data/stream/DataHydrator.js
+++ src/data/stream/DataHydrator.js
@@ -1,28 +1,58 @@
+const { get } = require('lodash');
 const Stream = require('stream');
 const ResultSet = require('./ResultSet');
 const ResultSetItem = require('./ResultSetItemProxy');
-const { map } = require('../../service/app.service');
+const { promiseChain, mapPromise, toKeyObj } = require('../../service/app.service');
 
 module.exports = class DataHydrator {
   constructor(query, data) {
-    const { model } = query.toObject();
-    const fields = model.getFields().filter(f => f.getName() !== 'id');
-    return data instanceof Stream ? DataHydrator.hydrate(query, fields, data) : new ResultSet(query, map(data, d => new ResultSetItem(query, d)));
+    let { select = {} } = query.toObject();
+    select = toKeyObj(select);
+    return data instanceof Stream ? DataHydrator.stream(query, data, select) : DataHydrator.process(query, data, select);
   }
 
-  static hydrate(query, fields, stream) {
-    const results = [];
+  static stream(query, stream, select) {
+    const promises = [];
 
     return new Promise((resolve, reject) => {
       stream.on('data', (data) => {
-        results.push(new ResultSetItem(query, data, fields));
+        promises.push(DataHydrator.hydrate(query, data, select));
       });
 
       stream.on('error', reject);
 
       stream.on('end', () => {
-        resolve(new ResultSet(query, results));
+        Promise.all(promises).then(results => resolve(new ResultSet(query, results)));
       });
+    });
+  }
+
+  static process(query, data, select) {
+    return mapPromise(data, d => DataHydrator.hydrate(query, d, select)).then(results => new ResultSet(query, results));
+  }
+
+  static hydrate(query, data, select) {
+    const loopArray = Array.from(new Array(Math.max(0, ...Object.keys(select).map(key => key.split('.').length))));
+
+    return new Promise((resolve, reject) => {
+      const item = new ResultSetItem(query, data);
+
+      return Promise.all(Object.keys(select).map((path) => {
+        const arrPath = path.split('.');
+
+        return Promise.all(loopArray.map((el, depth) => {
+          const arr = arrPath.slice(0, depth);
+          const $arr = arr.map(ele => `$${ele}`);
+          const key = arr[depth];
+
+          // id has special handling
+          if (!key || key === 'id') return Promise.resolve();
+
+          // Resolve all other attributes
+          get(item, arr.join('.'));
+          return promiseChain($arr.map($prop => chain => (chain.pop() || item)[$prop]()));
+        }));
+      })).then(() => resolve(item)).catch(e => reject(e));
     });
   }
 };
