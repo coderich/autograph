21c3f0c0c3818d6651bde74a172c1c1d1ad9bd8a
Richard Livolsi <richard.livolsi@goziohealth.com>
2024-02-17 15:41:58 -0500

    Removed unused block of code

--- src/service/schema.service.js
+++ src/service/schema.service.js
@@ -3,53 +3,6 @@
 // https://graphql.org/graphql-js/utilities/
 
 const { uniqWith } = require('lodash');
-const { GraphQLScalarType, GraphQLObjectType, GraphQLInterfaceType, GraphQLUnionType, GraphQLEnumType, GraphQLInputObjectType } = require('graphql');
-
-exports.getSchemaData = (schema) => {
-  const operations = ['Query', 'Mutation', 'Subscription'];
-
-  return Object.entries(schema.getTypeMap()).reduce((prev, [key, value]) => {
-    let type;
-
-    if (value instanceof GraphQLScalarType) {
-      type = 'scalars';
-    } else if (value instanceof GraphQLEnumType) {
-      type = 'enums';
-    } else if (value instanceof GraphQLUnionType) {
-      type = 'unions';
-    } else if (value instanceof GraphQLInterfaceType) {
-      type = 'interfaces';
-    } else if (value instanceof GraphQLInputObjectType) {
-      type = 'inputs';
-    } else if (value instanceof GraphQLObjectType) {
-      if (operations.includes(key)) {
-        type = 'operations';
-      } else {
-        type = 'models';
-      }
-    }
-
-    if (type) {
-      if (!key.startsWith('__')) {
-        prev[type][key] = value;
-      }
-    } else {
-      console.log(`Unknown schema type { ${key}: ${value} }`);
-    }
-
-    return prev;
-  }, {
-    enums: {},
-    models: {},
-    inputs: {},
-    unions: {},
-    scalars: {},
-    operations: {},
-    directives: {},
-    interfaces: {},
-    enumerations: {},
-  });
-};
 
 exports.identifyOnDeletes = (models, parentModel) => {
   return models.reduce((prev, model) => {
